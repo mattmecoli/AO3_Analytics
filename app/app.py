f54c960f4e115e9cb6e87e5f8040668ab2640bf6
Mathew Mecoli <mattmecoli@Mathews-Air.lan>
2021-04-28 14:11:35 -0400

    Initial commit - basic EDA and some notes

---
+++ app/app.py
@@ -0,0 +1,32 @@
+
+
+import time
+import random
+import requests
+from bs4 import BeautifulSoup
+
+
+
+url_base = "https://archiveofourown.org/works/"
+current_ix =  0
+streak_404_cnt = 0
+
+
+
+
+
+
+url =
+page = requests.get(url)
+
+
+
+
+
+
+
+
+
+rand_int = random.randint(10, 30)
+print('Downloaded {}.\n Waiting {} seconds.\n\n'.format(extra_validation_book_names[i], rand_int))
+time.sleep(rand_int)
